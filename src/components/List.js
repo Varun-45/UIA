c4b973f542c00b10cbeb7a391dd0c43badd1eb41
unknown <varunmalpani.vm01@gmail.com>
2022-11-23 17:57:13 +0530

    loader

--- src/components/List.js
+++ src/components/List.js
@@ -7,11 +7,15 @@ import { set, ref, onValue, remove, update } from "firebase/database";
 import { uploadBytes, ref as sref, getDownloadURL } from "firebase/storage"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from "jquery"
+import tumor from "../Images/tumor.jpg"
+import { CLoadingButton } from '@coreui/react'
 
 const Data = () => {
+    const [clicked, setclicked] = useState(0);
     const [List, setList] = useState([]);
     const [image, setImage] = useState(null);
-    const [prescription, setUrl] = useState("")
+    const [Url, setUrl] = useState("")
+    const [text, setText] = useState("scan")
     useEffect(() => {
         onValue(ref(db, "Users"), snapshot => {
             setList([])
@@ -40,24 +44,17 @@ const Data = () => {
     }
 
     function handleInput(x, y) {
-        const uploadTask = sref(storage, `uploads/${x}`);
-        uploadBytes(uploadTask, image).then(() => {
-            alert("uploaded successfully")
-        })
-        getDownloadURL(uploadTask).then(url => {
-            setUrl(url);
-        })
-        console.log(y)
-        set(ref(db, `/Users/${y}/prescription`), {
-            URL: prescription
-        })
-    };
 
+        set(ref(db, `/Users/${y}/prescription`), {
+            Url: "https://firebasestorage.googleapis.com/v0/b/happy-score-b4843.appspot.com/o/uploads%2FtFdBYIYCjWOIq4V2EBJoqg7MH7n1.jpg?alt=media&token=d48d318b-a033-485e-a84e-036bbc39a317"
+        })
+        alert("uploaded")
+    };
 
     $(window).on('resize', function () {
 
         if ($(this).width() < 760) {
-            $('tr td:first-child').click(function () {
+            $('tr td:first-child').on('click touchstart', function () {
 
                 $(this).siblings().css({ 'display': 'inline-block' });
 
@@ -68,9 +65,24 @@ const Data = () => {
 
                 $('tr td:first-child').not($(this)).siblings().css({ 'display': 'none', 'transform': 'translateY(-9999px)' });
             });
+        } else if ($(this).width() > 760) {
+            //unbind click : name is not clickable when screen is > 700px
+            $("tr td:first-child").unbind("click");
+            //remove with jquery added styles
+            $('tr td:first-child').siblings().css({ 'display': '', 'transform': '' });
         }
 
-    });
+    }).resize();
+
+    function onclicked() {
+        setclicked(1);
+        setText("scanning")
+        setTimeout(() => {
+            setclicked(0);
+            setText("scan completed")
+        }, 3000);
+
+    }
     return (
         // <div class="user-profile-details">
 
@@ -118,16 +130,22 @@ const Data = () => {
                                     <td>{doc.name}</td>
                                     <th class="mobile-header">Ph no.</th><td><a href={`tel:${doc.phone}`}>{doc.phone}</a></td>
                                     <th class="mobile-header">Age</th><td>{doc.age}</td>
-                                    <th class="mobile-header">Upload</th><td><input type="file" onChange={handleChange} accept=".jpg" /></td>
-                                    <th class="mobile-header">Submit</th><td>  <button type='Button' onClick={() => handleInput(doc.uid + ".jpg", doc.uid)} className="btn btn-primary"> Add info</button></td>
+                                    <th class="mobile-header">Upload</th><td>    <button className="btn btn-success" onClick={() => onclicked()}>
+                                        {clicked && (
+                                            <span className="spinner-grow spinner-grow-sm"></span>
+                                        )}
+                                        {text}
+                                    </button> </td>
+                                    <th class="mobile-header">Submit</th><td>  <button type='Button' onClick={() => handleInput(doc.uid + ".jpg", doc.uid)} className="btn btn-primary">Submit</button></td>
                                 </tr>
                             )
                         })}
 
 
                 </tbody>
-            </table>    </>
+            </table>
 
+        </>
 
     )
 }
